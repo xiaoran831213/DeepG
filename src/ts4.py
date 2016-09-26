ba266bb80e62095654c04d224f077e026f9d1b3e
xtong <tongxia1@msu.edu>
2016-09-26 18:28:10 -0400

    continue with ordinal perceptron adjustment.

--- src/ts4.py
+++ src/ts4.py
@@ -10,50 +10,24 @@ from rdm.trainer import R1, R2, CE, L1, L2
 from pdb import set_trace
 
 
-def get_dat1(m=256, f='../raw/wgs/03.vcf.gz'):
+def get_dat2(m=256, f='../raw/wgs/03.vcf.gz', d='012'):
     """ get dosage data """
     from gsq.vsq import DsgVsq
     from random import randint
     pos = randint(0, 10000000)
-    # raw dosage value in {0, 1, 2}
-    itr = DsgVsq(f, bp0=pos, wnd=m, dsg='011')
+
+    itr = DsgVsq(f, bp0=pos, wnd=m, dsg=d)
     dat = next(itr)
 
     idx = np.random.permutation(dat.shape[0])
     dat = dat[idx, ]
-    return np.array(dat, 'i4')
+    prb = np.array(dat, 'i4') / 2
 
-
-def get_dat2(m=256, f='../raw/wgs/03.vcf.gz'):
-    """ get dosage data """
-    from gsq.vsq import DsgVsq
-    from random import randint
-    pos = randint(0, 10000000)
-    # raw dosage value in {0, 1, 2}
-    itr = DsgVsq(f, bp0=pos, wnd=m, dsg='012')
-    dat = next(itr)
-
-    idx = np.random.permutation(dat.shape[0])
-    dat = dat[idx, ]
-    return np.array(dat, 'i4') / 2
-
-
-def get_dat3(m=256, f='../raw/wgs/03.vcf.gz'):
-    """ get dosage data """
-    from gsq.vsq import DsgVsq
-    from random import randint
-    pos = randint(0, 18000000)
-
-    itr = DsgVsq(f, bp0=pos, wnd=m, dsg='012')
-    dat = next(itr)
-
-    idx = np.random.permutation(dat.shape[0])
-    dat = dat[idx, ]
-
-    ret = np.ndarray((3, dat.shape[0], dat.shape[1]), dtype='f4')
+    bno = np.ndarray((3, dat.shape[0], dat.shape[1]), dtype='f4')
     for i in np.arange(3):
-        ret[i, ] = dat == i
-    return ret
+        bno[i, ] = dat == i
+    
+    return dat, prb, bno
 
 
 def get_SDA(dm):
@@ -74,60 +48,20 @@ def get_SDA(dm):
     return stk
 
 
-def pre_train1(stk, dat, trainers=None, rep=1, nep=5, reg=R1, lmd=.1):
-    """ layer-wise pre-train."""
-    # the depth
-    dp = len(stk) // 2
-
-    # the trainers
-    if trainers is None:
-        tr = [None] * dp
-    else:
-        tr = trainers
-
-    # repeatitive pre-training
-    for r in range(rep):
-        di = dat
-        for i in range(dp):
-            # wire the encoder to its decoder counterpart
-            ec, dc = stk[i], stk[-1 - i]
-            ae = Cat(ec, dc)
-
-            # the trainer
-            if tr[i] is None:
-                tr[i] = Trainer(ae, di, di, reg=reg, lrt=.005, lmd=lmd)
-            else:
-                tr[i].src.set_value(di)
-                tr[i].dst.set_value(di)
-            tr[i].lmd.set_value(lmd)
-            tr[i].tune(nep)
-
-            # wire the data to the bottom of the tuple, the output
-            # on top is the training material for next layer
-            di = ec(di).eval()
-
-    return stk, tr
-
-
-def fine_tune1(stk, dat, trainer=None, rep=1, reg=R1, lmd=.1):
-    """ Fine tune the entire network."""
-    if trainer is None:
-        trainer = Trainer(stk, dat, dat, reg=reg, lrt=0.0005, lmd=lmd)
-    else:
-        trainer.src.set_value(dat)
-        trainer.dst.set_value(dat)
-    trainer.tune(rep)
-
-    return stk, trainer
-
-
-def data_thr():
-    d = get_dat3(m=256)
-    return (d[:, :1750, :], d[:, 1750:, :])
+def gen_thr():
+    d, p, b = get_dat2(m=256)
+    d1, d5 = d[..., :1750, :], d[..., 1750:, :]
+    p1, p5 = p[..., :1750, :], p[..., 1750:, :]
+    b1, b5 = b[..., :1750, :], b[..., 1750:, :]
+    spgz('../dat/d1.pgz', d1)
+    spgz('../dat/d5.pgz', d5)
+    spgz('../dat/p1.pgz', p1)
+    spgz('../dat/p5.pgz', p5)
+    spgz('../dat/b1.pgz', b1)
+    spgz('../dat/b5.pgz', b5)
 
 
 def lvl_out(x, lvl=2, a=20):
-    from theano import tensor as T
     l = lvl - 1
     t = x - T.arange(0, l, dtype='float32').reshape((l, 1, 1))
     i = T.nnet.sigmoid(a * t)
@@ -158,7 +92,7 @@ def tst4():
     from copy import deepcopy
     a2 = deepcopy(a1)
     t2 = Trainer(a2, d1, d1, err=CE, reg=R1, lmd=.0, lrt=0.005, bsz=10)
-    # a2[-1].s = flvl(2, 2.0)
+    a2[-1].s = flvl(2, 2.0)
 
     return a1, a2, t1, t2, d1, d5
 
@@ -194,15 +128,10 @@ def tst6():
     """ adaptive training for one autoendocer. """
     hlp.set_seed(None)
 
-    d1 = lpgz('../dat/d1.pgz')  # 3D indicators
-    d5 = lpgz('../dat/d5.pgz')  # 3D indicators
-
-    d2 = np.zeros((2, d1.shape[0], d1.shape[1]))
-    d2[0][d1 == 0] = 1
-    d2[1][d1 == 1] = 1
-    d6 = np.zeros((2, d5.shape[0], d5.shape[1]))
-    d6[0][d5 == 0] = 1
-    d6[1][d5 == 1] = 1
+    d1 = lpgz('../dat/d1.pgz')  # 2D dosages
+    d5 = lpgz('../dat/d5.pgz')  # 2D dosages
+    b1 = lpgz('../dat/b1.pgz')  # 3D indicators
+    b5 = lpgz('../dat/b5.pgz')  # 3D indicators
 
     dm = d1.shape[-1] * np.power(2.0, [0, 1])
     dm = np.array(dm, 'i4')
@@ -215,7 +144,7 @@ def tst6():
     a2 = deepcopy(a1)
     a2[+0] = Odr((dm[0], dm[1]), d2.shape[0], 0, w=a1[+0].w)
     a2[-1] = Odr((dm[1], dm[0]), d2.shape[0], 1, w=a1[-1].w)
-    t2 = Trainer(a2, d2, d2, err=Odr.CE, reg=R1, lmd=.0, lrt=0.005, bsz=10)
+    t2 = Trainer(a2, b1, b1, err=Odr.CE, reg=R1, lmd=.0, lrt=0.005, bsz=10)
 
     return a1, a2, t1, t2, d1, d2, d5, d6
 
